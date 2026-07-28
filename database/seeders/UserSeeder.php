e246431abd622adc3affd1e2b37961876675aee3
Muhammad Ridho Alfarod <muhridhoalfarod@gmail.com>
2026-07-29 04:06:00 +0800

    Set email_verified_at in UserSeeder to bypass email verification on seeded admin account

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'username' => 'admin111',
             'email' => 'orchid.bpn@gmail.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('password123'),
             'role' => 'superadmin', 
             'status'=> 'aktif',
